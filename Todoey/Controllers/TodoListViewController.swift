641df110e43778ee402a64cd7fb145f5cb9487bd
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2021-06-18 17:51:15 +0300

    AppBrewery lesson 247 - Decoding Data with NSCoder. Using the stored .plist to retrieve the itemArray list.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -12,71 +12,62 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-
     
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         print(dataFilePath)
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-
-        let newItem3 = Item()
-        newItem3.title = "Fight Demogorgon"
-        itemArray.append(newItem3)
+        loadItems()
         
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itemArray = items
-//        }
+        //        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+        //            itemArray = items
+        //        }
     }
-
+    
     // MARK: - TableView DataSource Methods
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
-
-
+        
+        
         //ternary operator refactored into code
         cell.accessoryType = item.done ? .checkmark : .none
-            
         
-
+        
+        
         return cell
     }
-
+    
     // MARK: - TableView Delegate Methods
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-
+        
         // deselecting the selected row(removing the gray selection)
         tableView.deselectRow(at: indexPath, animated: true)
         
         saveItems()
     }
-
+    
     // MARK: - Add New Items
-
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
         var textField = UITextField()
-
+        
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "Add Item", style: .default) { _ in
             
             // what will happen once the user clicks the Add New Todoey Item on our UIAlert
@@ -84,7 +75,7 @@ class TodoListViewController: UITableViewController {
             let newItem = Item()
             
             newItem.title = textField.text!
-
+            
             self.itemArray.append(newItem)
             self.saveItems()
             
@@ -99,7 +90,9 @@ class TodoListViewController: UITableViewController {
     //MARK: - Model Manipulation Methods
     
     func saveItems() {
+        
         let encoder = PropertyListEncoder()
+        
         do {
             let data = try encoder.encode(self.itemArray)
             try data.write(to: self.dataFilePath!)
@@ -109,7 +102,18 @@ class TodoListViewController: UITableViewController {
         }
         self.tableView.reloadData()
     }
-
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding itemArray, \(error)")
+            }
+        }
+    }
+    
 }
 
 // singletons and a note about the next lesson has overviewed in 17.06.21 - 20.00
